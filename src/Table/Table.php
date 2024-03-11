559ebdc672368feee7452d5fb8c8083f799b62e7
jdw5 <joshuadwallace1@gmail.com>
2024-03-11 19:14:55 +1000

    Documentation on table

--- src/Table/Table.php
+++ src/Table/Table.php
@@ -2,6 +2,7 @@
 
 namespace Jdw5\Vanguard\Table;
 
+use Illuminate\Database\Eloquent\Builder;
 use Jdw5\Vanguard\Primitive;
 use Jdw5\Vanguard\Concerns\HasId;
 use Jdw5\Vanguard\Concerns\HasScope;
@@ -9,36 +10,47 @@ use Jdw5\Vanguard\Concerns\HasActions;
 use Jdw5\Vanguard\Table\Concerns\HasKey;
 use Jdw5\Vanguard\Table\Contracts\Tables;
 use Jdw5\Vanguard\Concerns\HasRefinements;
+use Jdw5\Vanguard\Table\Concerns\HasModel;
 use Jdw5\Vanguard\Table\Concerns\HasColumns;
 use Jdw5\Vanguard\Table\Concerns\HasRecords;
-use Jdw5\Vanguard\Eloquent\Concerns\HasModel;
-use Illuminate\Database\Eloquent\Builder;
 use Jdw5\Vanguard\Table\Concerns\HasPagination;
-use Illuminate\Database\Eloquent\Collection;
 use Jdw5\Vanguard\Table\Exceptions\InvalidKeyException;
 
 abstract class Table extends Primitive implements Tables
 {
-    use HasColumns,
-        HasActions,
-        HasScope,
-        HasId,
-        HasModel,
-        HasPagination,
-        HasRecords,
-        HasRefinements,
-        HasKey;
+    use HasColumns;
+    use HasActions;
+    use HasScope;
+    use HasId;
+    use HasModel;
+    use HasPagination;
+    use HasRecords;
+    use HasRefinements;
+    use HasKey;
 
     public function __construct(?Builder $data = null)
     {
         $this->query($data);
     }
 
+    /**
+     * Create a new table instance.
+     * 
+     * @param Builder|null $data
+     * @return static
+     */
     public static function make(?Builder $data = null): static
     {
         return new static($data);
     }
 
+    /**
+     * Get the key for the table.
+     * 
+     * @return string
+     * @throws InvalidKeyException
+     * @return string
+     */
     public function tableKey(): string 
     {
         try { 
@@ -48,6 +60,11 @@ abstract class Table extends Primitive implements Tables
         }
     }
 
+    /**
+     * Serialize the table to JSON.
+     * 
+     * @return array
+     */
     public function jsonSerialize(): array
     {
         return [
