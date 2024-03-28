76e17b1d7713d656a6899cab24d31815d8b07cfc
jdw5 <joshuadwallace1@gmail.com>
2024-03-28 17:39:18 +1000

    Working through rebuild of pagination API

--- src/Table/Concerns/HasPagination.php
+++ src/Table/Concerns/HasPagination.php
@@ -21,6 +21,9 @@ trait HasPagination
     protected $page = null;
     protected $columns = ['*'];
 
+    private bool $set = false;
+    protected $defaultPerPage = 10;
+    protected $paginateType = null;
     protected $showKey = 'show';
 
     /**
@@ -32,15 +35,14 @@ trait HasPagination
      * @param int|null $page
      * @return static
      */
-    public function paginate($perPage = 10, $columns = ['*'], $pageName = 'page', $page = null): static
+    public function paginate(int $perPage = 10, array $columns = ['*'], string $pageName = 'page', $page = null): static
     {
-        $this->paginateConfig = PaginateConfiguration::paginate([
-            'perPage' => $perPage,
-            'columns' => $columns,
-            'pageName' => $pageName,
-            'page' => $page,
-        ]);
-
+        $this->perPage = $perPage;
+        $this->columns = $columns;
+        $this->pageName = $pageName;
+        $this->page = $page;
+        $this->paginateType = 'paginate';
+        $this->set();
         return $this;
     }
     /**
@@ -52,25 +54,46 @@ trait HasPagination
      * @param int|null $cursor
      * @return static
      */
-    public function cursorPaginate($perPage = 10, $columns = ['*'], $cursorName = 'cursor', $cursor = null): static
+    public function cursorPaginate(int $perPage = 10, array $columns = ['*'], string $cursorName = 'cursor', $cursor = null): static
     {
-        $this->paginateConfig = PaginateConfiguration::cursor([
-            'perPage' => $perPage,
-            'columns' => $columns,
-            'pageName' => $cursorName,
-            'page' => $cursor,
-        ]);
+        $this->perPage = $perPage;
+        $this->columns = $columns;
+        $this->pageName = $cursorName;
+        $this->page = $cursor;
+        $this->paginateType = 'paginate';
+        $this->set();
         return $this;
     }
 
+    public function get(): static
+    {
+        $this->paginateType = null;
+        $this->set();
+        return $this;
+    }
+
+    
+    public function dynamicPaginate(array $perPages, string $showKey = 'show'): static
+    {
+        $this->perPage = $perPages;
+        $this->showKey = $showKey;
+        $this->paginateType = 'dynamic';
+        $this->set();
+        return $this;
+    }
+    
+    private function set(): void
+    {
+        $this->set = true;
+    }
     /**
      * Check if the table is paginated
      * 
      * @return bool
      */
-    public function isPaginated(): bool 
+    public function hasPagination(): bool 
     {
-        return !\is_null($this->paginateConfig);
+        return !\is_null($this->paginateType) && $this->set;
     }
 
     /**
@@ -80,7 +103,7 @@ trait HasPagination
      */
     public function paginateType(): string|null
     {
-        return $this->paginateConfig->type->value;
+        return $this->paginateType;
     }
 
     /**
@@ -98,7 +121,41 @@ trait HasPagination
         ];
     }
 
-    // public function 
+    public function getDynamicPerPage()
+    {
+        $request = request();
+        // Retrieve the show search query parameter
+
+        // Verify it's in perPages; prevent users from controlling. If it isn't, then, return the defaultPerPage
+
+    }
+
+    public function perPage()
+    {
+
+        if ($this->set() && $this->paginateType() !== 'dynamic') {
+            return $this->perPage;
+        }
+        if ($this->paginateType() === 'dynamic') {
+            return $this->getDynamicPerPage();
+        }
+        return $this->perPage;
+    }
+
+    public function getPagination(): array
+    {
+        // The application defaults to pagination with the defaultPerPage if nothing is provided
+        return [
+            'perPage' => $this->perPage,
+            'pageName' => $this->pageName,
+            'page' => $this->page,
+            'columns' => $this->columns,
+        ];
+    }
+
+    /**
+     * Should be used to override the perPage metric
+     */
     public function definePagination(): mixed
     {
         return 10;
