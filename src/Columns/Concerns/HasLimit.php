b06a7af2632ef635f4fea89a1491f6b76efc1aba
jdw5 <joshuadwallace1@gmail.com>
2024-07-29 12:26:00 +1000

    New column concerns

---
+++ src/Columns/Concerns/HasLimit.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+namespace Conquest\Table\Columns\Concerns;
+
+use Closure;
+
+trait HasLimit
+{
+    protected int|Closure|null $limit = null;
+
+    public function limit(int|Closure $limit): static
+    {
+        $this->setLimit($limit);
+        return $this;
+    }
+
+    public function setLimit(int|Closure|null $limit): void
+    {
+        if (is_null($limit)) return;
+        $this->limit = $limit;
+    }
+
+    public function hasLimit(): bool
+    {
+        return !$this->lacksLimit();
+    }
+
+    public function lacksLimit(): bool
+    {
+        return is_null($this->limit);
+    }
+
+    public function getLimit(): ?int
+    {
+        return $this->evaluate(
+            value: $this->limit,
+        );
+    }
+}
---
+++ src/Columns/Concerns/HasTooltip.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+namespace Conquest\Table\Columns\Concerns;
+
+use Closure;
+
+trait HasTooltip
+{
+    protected string|Closure|null $tooltip = null;
+
+    public function tooltip(string|Closure $tooltip): static
+    {
+        $this->setTooltip($tooltip);
+        return $this;
+    }
+
+    public function setTooltip(string|Closure|null $tooltip): void
+    {
+        if (is_null($tooltip)) return;
+        $this->tooltip = $tooltip;
+    }
+
+    public function hasTooltip(): bool
+    {
+        return !$this->lacksTooltip();
+    }
+
+    public function lacksTooltip(): bool
+    {
+        return is_null($this->tooltip);
+    }
+
+    public function getTooltip(): ?string
+    {
+        return $this->evaluate(
+            value: $this->tooltip,
+        );
+    }
+}
